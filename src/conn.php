fd7586511efd0f679a45752fa605d668c2d3de26
ibrahimaseckdieng <62561279+ibrahimaseckdieng@users.noreply.github.com>
2021-12-02 03:43:12 +0100

    Update conn.php

--- src/conn.php
+++ src/conn.php
@@ -1,7 +1,7 @@
 <?php
 	try
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=quizz_odc', 'root', '');
+		$bdd = new PDO('mysql:host=databases.000webhost.com;dbname=id18048285_quizzbd', 'id18048285_seckdieng', 'uW75pCEz9PvvUN=j');
 	}catch (Exception $e)
 	{
 		die('Erreur : ' . $e->getMessage());
